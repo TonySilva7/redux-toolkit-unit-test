f424feca31ab5d6ff1bc5c1ef6d00c77d5b0287b
TonySilva7 <tony.deve7oper@gmail.com>
2021-09-22 19:39:28 -0300

    style: create container

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,7 @@
 import BarTitle from '../BarTitle';
 import Footer from '../Footer';
 import Header from '../Header';
-import SearchBar from '../SearchBar';
+import MainContent from '../MainContent';
 import { Wrapper } from './styles';
 
 export default function Main() {
@@ -9,7 +9,7 @@ export default function Main() {
 		<Wrapper>
 			<Header />
 			<BarTitle />
-			<SearchBar />
+			<MainContent />
 			<Footer />
 		</Wrapper>
 	);
---
+++ src/components/MainContent/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.footer`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	/* justify-content: center; */
+	align-self: center;
+	padding: 2rem;
+
+	width: 97%;
+	height: 60rem;
+
+	background: rgba(255, 255, 255, 0.7);
+	box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.04);
+	border-radius: 10px;
+
+	h1 {
+		width: 40%;
+		font-weight: normal;
+		color: #466eb6;
+		text-align: center;
+	}
+`;
